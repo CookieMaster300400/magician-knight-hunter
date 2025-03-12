a80fb656896f3801dba7c2bc1bf9a208b439f081
CookieMaster300400 <130558675+CookieMaster300400@users.noreply.github.com>
2025-03-12 19:33:13 +0200

    Update Box.cs

--- ConsoleApp1/ConsoleApp1/Box.cs
+++ ConsoleApp1/ConsoleApp1/Box.cs
@@ -1,46 +1,81 @@
-﻿namespace ConsoleApp1
+namespace ConsoleApp1
 {
     class Box : Item
     {
-        public static int BoxSize()
+        public static string[] box { get; private set; }
+        public static void SetBoxSize()
         {
-            const int MinBoxSize = 6;
-            const int MaxBoxSize = 20;
-            while (true)
+            const byte MinBoxSize = 6;
+            const byte MaxBoxSize = 20;
+            while(true)
             {
-                if (int.TryParse(Console.ReadLine(), out int numberOfCells) && numberOfCells >= MinBoxSize && numberOfCells <= MaxBoxSize && numberOfCells % 2 == 0)
-                    return numberOfCells;
+                Console.WriteLine($"Сколько ячеек будет в сундуке?\nНе короче {MinBoxSize}\nНе длиннее {MaxBoxSize}\nКоличество должно быть четным");
+                if(int.TryParse(Console.ReadLine(), out int cells) && cells >= MinBoxSize && cells <= MaxBoxSize && cells % 2 == 0)
+                {
+                    box = new string[cells];
+                    break;
+                }
+                Console.Clear();
             }
         }
-        public string[] box { get; init; }
-        public Box(int numberOfCells, int keyCount = 0, int healCount = 0, int knifeCount = 0) : base(keyCount, knifeCount, healCount)
+        private protected static int _keyCount;
+        private protected static int _knifeCount;
+        private protected static int _healCount;
+        public static void FillTheBox(int key, int knife, int heal)
         {
-            box = new string[numberOfCells];
-            for (int i = 0; i < numberOfCells; i++)
+            for (int i = 0; i < box.Length; i++)
             {
-                if (keyCount > 0)
+                if (key > 0 && key < maxValue)
                 {
-                    box[i] = keySymbol;
-                    KeyCount = keyCount;
-                    keyCount = 0;
+                    _keyCount = key;
+                    key = 0;
+                    box[i] = KeySymbol;
                 }
-                else if (knifeCount > 0)
+                else if (knife > 0 && knife < maxValue)
                 {
-                    box[i] = knifeSymbol;
-                    KnifeCount = knifeCount;
-                    knifeCount = 0;
+                    _knifeCount = knife;
+                    knife = 0;
+                    box[i] = KnifeSymbol;
                 }
-                else if (healCount > 0)
+                else if (heal > 0 && heal < maxValue)
                 {
-                    box[i] = healSymbol;
-                    HealCount = healCount;
-                    healCount = 0;
+                    _healCount = heal;
+                    heal = 0;
+                    box[i] = HealSymbol;
                 }
                 else
                 {
-                    box[i] = "[ ]";
+                    box[i] = EmptyPlace;
                 }
             }
+        }
+        public Box() : base()
+        {
+            FillTheBox(3, 1, 2);
+        }
+        public static void ShowBox()
+        {
+            byte j = 0;
+            Console.Write("\t");
+            for (int i = 0; i < box.Length; i++)
+            {
+                Console.Write(box[i]);
+                ++j;
+                if (j == box.Length / 2)
+                {
+                    Console.Write("\n\t");
+                    j = 0;
+                }
+            }
+        }
+        public static int[] Remove()
+        {
+            int[] keysKnifesHeals = { _keyCount, _knifeCount, _healCount };
+            _keyCount = 0;
+            _knifeCount = 0;
+            _healCount = 0;
+            FillTheBox(0, 0, 0);
+            return keysKnifesHeals;
         }
     }
 }
