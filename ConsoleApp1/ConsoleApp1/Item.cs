86ebc1554fa08bfe5bc7e5922eb1a11052ea9be2
CookieMaster300400 <130558675+CookieMaster300400@users.noreply.github.com>
2025-03-12 19:34:09 +0200

    Update Item.cs

--- ConsoleApp1/ConsoleApp1/Item.cs
+++ ConsoleApp1/ConsoleApp1/Item.cs
@@ -1,52 +1,12 @@
-﻿namespace ConsoleApp1
+namespace ConsoleApp1
 {
     abstract class Item
     {
-        public static string keySymbol = "[1]";
-        public static string knifeSymbol = "[>]";
-        public static string healSymbol = "[$]";
-        private const int MaxValue = 64;
-        private static int _key;
-        public static int KeyCount 
-        {
-            get => _key;
-            protected set
-            {
-                if (value <= MaxValue)
-                {
-                    _key = value;
-                }
-            }
-        }
-        private static int _knife;
-        public static int KnifeCount
-        {
-            get => _key;
-            protected set
-            {
-                if (value <= MaxValue)
-                {
-                    _knife = value;
-                }
-            }
-        }
-        private static int _healCount;
-        public static int HealCount
-        {
-            get => _key;
-            protected set
-            {
-                if (value <= MaxValue)
-                {
-                    _healCount = value;
-                }
-            }
-        }
-        public Item(int key, int knife, int heal)
-        {
-            KeyCount = key;
-            KnifeCount = knife;
-            HealCount = heal;
-        }
+        public static readonly string KeySymbol = "[1]";
+        public static readonly string KnifeSymbol = "[>]";
+        public static readonly string HealSymbol = "[$]";
+        public static readonly string EmptyPlace = "[ ]";
+
+        public static readonly int maxValue = 10;
     }
 }
